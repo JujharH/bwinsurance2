35623ef52ef38ad177322625452a626855e9e8ae
Aron Beal <aron@hottomali.com>
2019-10-28 13:40:44 -0700

    (develop) Adds fixture support
    https://app.asana.com/0/1132578874282309/1132590387550875
    - Adds support for fixtures for setting up the local dev environment.

---
+++ web/modules/bwinsurance_dev/src/DevelFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Drupal\bwinsurance_dev;
+
+
+use Drupal\content_fixtures\Fixture\AbstractFixture;
+
+class DevelFixtures extends AbstractFixture {
+
+  /**
+   * Load fixture.
+   *
+   * @return void
+   */
+  public function load() {
+    // TODO: Implement load() method.
+  }
+}
